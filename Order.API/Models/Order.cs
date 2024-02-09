a3a1d3c43f80ef18faaefeb33cc1965565f43e0e
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-02-09 15:49:13 +0300

    SagaPattern 1

---
+++ Order.API/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Order.API.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public string BuyerId { get; set; }
+        public Address Address { get; set; }
+
+        public ICollection<OrderItem> Items { get; set; }= new List<OrderItem>();
+        public OrderStatus Status { get; set; }
+        public string FailMessage { get; set; }
+    }
+
+    public enum OrderStatus
+    {
+        Suspend,
+        Success,
+        Fail
+    }
+}
