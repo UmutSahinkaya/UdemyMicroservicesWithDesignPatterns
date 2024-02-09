7b46107178103d615aec109790855db7a051f2e0
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-02-09 17:43:30 +0300

    Order.API Masstransit and RabbitMQ konfigürasyonu yapıldı.

---
+++ Order.API/DTOs/OrderCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Order.API.DTOs
+{
+    public class OrderCreateDto
+    {
+        public string BuyerId { get; set; }
+        public List<OrderItemDto> OrderItems { get; set; }
+        public PaymentDto Payment { get; set; }
+        public AddressDto Address { get; set; }
+    }
+}
