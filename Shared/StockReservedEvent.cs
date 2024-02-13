4cd15b0eb4e1d8ed6dbfb6ef8be0660b53400510
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-02-13 11:49:04 +0300

    Shared içindeki event ler

--- Shared/StockReservedEvent.cs
+++ Shared/StockReservedEvent.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 
 namespace Shared
 {
-    public class OrderCreatedEvent
+    public class StockReservedEvent
     {
         public int OrderId { get; set; }
         public string BuyerId { get; set; }
 
         public PaymentMessage Payment { get; set; }
+
         public List<OrderItemMessage> OrderItems { get; set; } = new List<OrderItemMessage>();
     }
 }
