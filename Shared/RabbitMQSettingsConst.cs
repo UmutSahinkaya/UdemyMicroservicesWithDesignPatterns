40e2613cb8e54a5027af8e65cc12ed7786749c4d
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-02-16 17:44:23 +0300

    16. Stock API (Microservice) -4 Choreograpfy is over...

--- Shared/RabbitMQSettingsConst.cs
+++ Shared/RabbitMQSettingsConst.cs
@@ -12,7 +12,7 @@ namespace Shared
 
         public const string StockOrderCreatedEventQueueName = "stock-order-created-queue";
 
-        //public const string StockPaymentFailedEventQueueName = "stock-payment-failed-queue";
+        public const string StockPaymentFailedEventQueueName = "stock-payment-failed-queue";
 
         public const string OrderPaymentCompletedEventQueueName = "order-payment-completed-queue";
         public const string OrderPaymentFailedEventQueueName = "order-payment-failed-queue";
--- Stock.API/Consumers/PaymentFailedEventConsumer.cs
+++ Stock.API/Consumers/PaymentFailedEventConsumer.cs
@@ -1,4 +1,5 @@
 ﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Shared;
 using Stock.API.Models;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Stock.API.Consumers
 {
-    public class PaymentFailedEventConsumer /*: IConsumer<PaymentFailedEvent>*/
+    public class PaymentFailedEventConsumer : IConsumer<PaymentFailedEvent>
     {
         private readonly AppDbContext _context;
         private ILogger<PaymentFailedEventConsumer> _logger;
@@ -17,9 +18,19 @@ namespace Stock.API.Consumers
             _logger = logger;
         }
 
-        //public Task Consume(ConsumeContext<PaymentFailedEvent> context)
-        //{
-            
-        //}
+        public async Task Consume(ConsumeContext<PaymentFailedEvent> context)
+        {
+            foreach (var item in context.Message.orderItems)
+            {
+                var stock = await _context.Stocks.FirstOrDefaultAsync(x => x.ProductId == item.ProductId);
+
+                if(stock != null)
+                {
+                    stock.Count += item.Count;
+                    await _context.SaveChangesAsync();
+                }
+            }
+            _logger.LogInformation($"Stock was released for Buyer Id ({context.Message.orderId})");
+        }
     }
 }
