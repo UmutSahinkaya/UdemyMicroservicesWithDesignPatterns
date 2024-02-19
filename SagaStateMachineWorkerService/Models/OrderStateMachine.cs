3f586a0d101de0271f7c77f0665f53dea9cc6176
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-02-19 16:25:40 +0300

    6. Adım(OrderStateMachine)

--- SagaStateMachineWorkerService/Models/OrderStateMachine.cs
+++ SagaStateMachineWorkerService/Models/OrderStateMachine.cs
@@ -1,4 +1,5 @@
 ﻿using Automatonymous;
+using Shared.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,29 @@ namespace SagaStateMachineWorkerService.Models
 {
     public class OrderStateMachine:MassTransitStateMachine<OrderStateInstance>
     {
+        public Event<IOrderCreatedRequestEvent> OrderCreatedRequestEvent { get; set; }// Eventim
+
+        public State OrderCreated { get;private set; }
+
+
+        public OrderStateMachine()
+        {
+            InstanceState(x => x.CurrentState);
+
+            Event(() => OrderCreatedRequestEvent, y => y.CorrelateBy<int>(x => x.OrderId, z => z.Message.OrderId).SelectId(context => Guid.NewGuid()));
+            //İlgili state ilk aşamadayken
+            Initially(When(OrderCreatedRequestEvent).Then(context =>
+            {
+                context.Instance.BuyerId = context.Data.BuyerId;
+                context.Instance.OrderId = context.Data.OrderId;
+                context.Instance.CreatedDate = DateTime.Now;
+
+                context.Instance.CardName = context.Data.Payment.CardName;
+                context.Instance.CardNumber = context.Data.Payment.CardNumber;
+                context.Instance.CVV = context.Data.Payment.CVV;
+                context.Instance.Expiration = context.Data.Payment.Expiration;
+                context.Instance.TotalPrice = context.Data.Payment.TotalPrice;
+            }).Then(context => { Console.WriteLine($"OrderCreatedRequestEvent before : {context.Instance}"); }).TransitionTo(OrderCreated).Then(context => { Console.WriteLine($"OrderCreatedRequestEvent before : {context.Instance}"); }));
+        }
     }
 }
