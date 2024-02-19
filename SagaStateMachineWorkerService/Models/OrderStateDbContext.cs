3eb8927198090d65c3d159baf42d4873761fd1e1
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-02-19 14:37:12 +0300

    05. 4. Adım(OrderStateDbContext)

---
+++ SagaStateMachineWorkerService/Models/OrderStateDbContext.cs
@@ -0,0 +1,19 @@
+﻿using MassTransit.EntityFrameworkCoreIntegration;
+using MassTransit.EntityFrameworkCoreIntegration.Mappings;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace SagaStateMachineWorkerService.Models
+{
+    public class OrderStateDbContext : SagaDbContext
+    {
+        public OrderStateDbContext(DbContextOptions<OrderStateDbContext> options) : base(options)
+        {
+        }
+
+        protected override IEnumerable<ISagaClassMap> Configurations
+        {
+            get { yield return new OrderStateMap(); }
+        }
+    }
+}
