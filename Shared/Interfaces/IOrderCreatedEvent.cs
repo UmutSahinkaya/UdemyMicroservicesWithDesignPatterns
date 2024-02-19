f96a41abdcdc73b37d536b1e831ef5d98558d7b4
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-02-19 17:26:20 +0300

    8. Adım(OrderCreatedEvent)

---
+++ Shared/Interfaces/IOrderCreatedEvent.cs
@@ -0,0 +1,11 @@
+﻿using MassTransit;
+using System;
+using System.Collections.Generic;
+
+namespace Shared.Interfaces
+{
+    public interface IOrderCreatedEvent:CorrelatedBy<Guid>
+    {
+      public List<OrderItemMessage> OrderItems { get; set; }
+    }
+}
---
+++ Stock.API/Consumers/OrderCreatedEventConsumer.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit;
+using Shared.Interfaces;
+using System.Threading.Tasks;
+
+namespace Stock.API.Consumers
+{
+    public class OrderCreatedEventConsumer : IConsumer<IOrderCreatedEvent>
+    {
+        //private readonly
+        public Task Consume(ConsumeContext<IOrderCreatedEvent> context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
