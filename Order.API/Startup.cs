4c9575cfe33ee2c241e60e4a59d22fe0d1ac96fb
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-02-22 14:43:19 +0300

    12. 11. Adım (PaymentCompletedEvent)

--- Order.API/Startup.cs
+++ Order.API/Startup.cs
@@ -6,7 +6,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Order.API.Consumers;
 using Order.API.Models;
+using Shared;
 
 namespace Order.API
 {
@@ -24,11 +26,14 @@ namespace Order.API
         {
             services.AddMassTransit(x =>
             {
-                
+                x.AddConsumer<OrderRequestCompletedEventConsumer>();
                 x.UsingRabbitMq((context, cfg) =>
                 {
                     cfg.Host(Configuration.GetConnectionString("RabbitMQ"));
-                    
+                    cfg.ReceiveEndpoint(RabbitMQSettingsConst.OrderRequestCompletedEventQueueName, x =>
+                    {
+                        x.ConfigureConsumer<OrderRequestCompletedEventConsumer>(context);
+                    });
                 });
             });
             services.AddDbContext<AppDbContext>(options =>
--- SagaStateMachineWorkerService/Models/OrderStateMachine.cs
+++ SagaStateMachineWorkerService/Models/OrderStateMachine.cs
@@ -14,15 +14,21 @@ namespace SagaStateMachineWorkerService.Models
     {
         public Event<IOrderCreatedRequestEvent> OrderCreatedRequestEvent { get; set; }
         public Event<IStockReservedEvent> StockReservedEvent { get; set; }
+        public Event<IPaymentCompletedEvent> PaymentCompletedEvent { get; set; }
 
         public State OrderCreated { get; private set; }
         public State StockReserved { get; private set; }
+        public State PaymentCompleted { get; private set; }
 
         public OrderStateMachine()
         {
             InstanceState(x => x.CurrentState);
 
             Event(() => OrderCreatedRequestEvent, y => y.CorrelateBy<int>(x => x.OrderId, z => z.Message.OrderId).SelectId(context => Guid.NewGuid()));
+
+            Event(() => StockReservedEvent, x => x.CorrelateById(y => y.Message.CorrelationId));
+
+            Event(() => PaymentCompletedEvent, x => x.CorrelateById(y => y.Message.CorrelationId));
 
             Initially(
              When(OrderCreatedRequestEvent)
@@ -58,9 +64,16 @@ namespace SagaStateMachineWorkerService.Models
                         Expiration = context.Instance.Expiration,
                         TotalPrice = context.Instance.TotalPrice
                     },
-                    BuyerId= context.Instance.BuyerId,
+                    BuyerId = context.Instance.BuyerId,
                 })
                 .Then(context => { Console.WriteLine($"OrderCreatedRequestEvent After : {context.Instance}"); }));
+
+
+
+            During(StockReserved, When(PaymentCompletedEvent).TransitionTo(PaymentCompleted).Publish(context => new OrderRequestCompletedEvent()
+            {
+                OrderId = context.Instance.OrderId
+            }).Then(context => { Console.WriteLine($"PaymentCompletedEvent After : {context.Instance}"); }).Finalize());
         }
     }
 }
